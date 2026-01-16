8dceb9473cd979479090f4d31bed0abd56794826
Apeksha Shenoy <apekshashenoy.1992@gmail.com>
2026-01-16 16:17:44 +0100

    updates

--- Django_labs/community/blog/views.py
+++ Django_labs/community/blog/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, redirect
+from .models import Post, Comment
+from .forms import CommentForm
+from django.contrib.auth.decorators import login_required
 
 def post_list(request):
     posts = Post.objects.all()
-    return render(request, 'blog/post_list.html', {'posts': posts})
 
+    if request.method == 'POST':
+        if not request.user.is_authenticated:
+            return redirect('login')
+        post_id = request.POST.get('post_id')
+        post = Post.objects.get(id=post_id)
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect('post_list')
+    else:
+        form = CommentForm()
+
+    return render(request, 'blog/post_list.html', {'posts': posts, 'form': form})
